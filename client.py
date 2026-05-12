f1a765cb9a5279bcca0c090175606f7c04ca072b
tinkajob <birsa.job@gmail.com>
2026-05-12 20:19:18 +0200

    First working texting version.

--- client.py
+++ client.py
@@ -1,12 +1,32 @@
-import socket, pygame
+import socket, threading, pygame
 
-pygame.init()
-pygame.display.set_mode((800, 1200))
+# pygame.init()
+# pygame.display.set_mode((800, 1200))
+username = input("Enter your username: ")
+
+SERVER_IP = "127.0.0.1"
+PORT = 1234
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, PORT))
+client.send(username.encode())
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            print(f"{message}")
+        except: break
+    
+def send():
+    while True:
+        message = input()
+        client.send(message.encode())
+
+threading.Thread(target=receive, daemon=True).start()
+threading.Thread(target=send, daemon=True).start()
 
 
+while True: pass
 
 
-
-
-
-pygame.quit()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, threading
 
 HOST = "0.0.0.0"
 PORT = 1234 # Idk just sth
@@ -6,11 +6,37 @@ PORT = 1234 # Idk just sth
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
+print("Server is running!")
 
-client, addr = server.accept()
+clients = []
 
 
-  
+def handle_client(client, address):
+    clients.append(client)
+    username = client.recv(1024).decode()
+    print(f"{username} connected")
+
+    while True:
+        try:
+            data = client.recv(1024)
+            if not data: break
+            message = data.decode()
+            print(f"{username}: {message}")
+
+            for c in clients:
+                if c != client: c.send(f"{username}: {message}".encode())
+
+        except: break
+    
+    print(f"{username} disconnected")
+    clients.remove(client)
+    client.close()
+
+
+while True:
+    client, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(client, addr))
+    thread.start()
 
 
 client.close()
